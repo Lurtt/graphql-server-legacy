ac3824166d2de04ae690c5a4fdfa230c3a189e33
Lurtt <lurttik@gmail.com>
2017-11-21 22:57:43 +0100

    create mondodb connection

--- src/index.js
+++ src/index.js
@@ -3,17 +3,26 @@ import bodyParser from 'body-parser'
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express'
 
 import schema from './schema'
+import connectMongo from './mongo-connector'
 
-const app = express();
+const start = async () => {
+  const mongo = await connectMongo();
+  const app = express();
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({schema}))
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: {mongo},
+    schema
+  }))
 
-app.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql',
-}))
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }))
 
-const PORT = 3000
+  const PORT = 3000
 
-app.listen(PORT, () => {
-  console.log(`Hackernews GraphQL server running on port ${PORT}.`)
-})
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`)
+  })
+}
+
+start()
